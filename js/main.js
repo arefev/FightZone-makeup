f12803d12b1b1e2fc4f713c22381c5e9665b0369
Антон <diy9.work@gmail.com>
2015-01-12 20:18:29 +0300

    Первая выгрузка
    Первая выгрузка с пустым шаблоном

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// MAIN JavaScript
+
+$(function(){
+	
+	
+});
+
+$(window).resize(function(){
+	centerPopup($(".b-popup"));
+	
+});
+
+function showPopup(element)
+{
+	element.show();
+	$("#windowFill").show();
+	
+	centerPopup(element);
+}
+
+function hidePopup(element)
+{
+	if (!element.length)
+	{
+		element = $(".b-popup:visible");
+	}
+	
+	element.hide();
+	$("#windowFill").hide();
+}
+
+function centerPopup(element)
+{
+	var elWidth = element.width();
+	var elHeight = element.height();
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+	var scrollTop = $(window).scrollTop();
+	var left = windowWidth/2 - elWidth/2;
+	var top = windowHeight/2 - elHeight/2 + scrollTop;
+	
+	if (left < 0) left = 0;
+	if (top < 0) top = 0;
+	
+	element.css({
+		left:left,
+		top:top
+	});
+}
