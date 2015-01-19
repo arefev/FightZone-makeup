ab6f37dc79ed76063a20ab6a989ff7653a05b684
xwolfx <diy9@mail.ru>
2015-01-19 18:01:07 +0300

    JS главного слайдера

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function runAnimationSection(section) {
 
 function runAnimationBlock(animationBlock) {
 	var animationTimeOut = parseInt(animationBlock.data("animation-timestart")),
-		animationTimePlus = 300;
+		animationTimePlus = 100;
 		
 	if (isNaN(animationTimeOut))
 		animationTimeOut = 0;
@@ -30,6 +30,49 @@ function runAnimationBlock(animationBlock) {
 $(function(){
 	runAnimationSection($(".header"));
 	runAnimationSection($(".main-slider"));
+	
+	$(".main-slider__arrow_right, .main-slider__arrow_left").click(function(){
+		var textBlock = $(".main-slider__text:visible");
+			
+			
+		if ($(this).is(".main-slider__arrow_right")) {
+			var textBlockNext = textBlock.next(".main-slider__text");
+			if (!textBlockNext.length) {
+				textBlockNext = $(".main-slider__text").first();
+			}
+		} else {
+			var textBlockNext = textBlock.prev(".main-slider__text");
+			if (!textBlockNext.length) {
+				textBlockNext = $(".main-slider__text").last();
+			}
+		}
+			
+		textBlock.data({
+			"animation-class": "slideRightFadeOut",
+			"animation-timestart": 0
+		});
+		
+		textBlockNext.data({
+			"animation-class": "slideLeft",
+			"animation-timestart": 0
+		});
+		
+		textBlock.find(".animation").removeClass("slideRightFadeOut");
+		textBlockNext.find(".animation").removeClass("slideRightFadeOut slideLeft");
+		
+		
+		var size = Math.abs(textBlock.find(".animation").size() - 2),
+			timeOut = 300 * size;
+	
+		runAnimationBlock(textBlock);
+		setTimeout(function(){
+			textBlock.hide();
+			textBlockNext.show();
+			runAnimationBlock(textBlockNext);
+			
+		}, timeOut);
+		
+	});
 });
 
 
